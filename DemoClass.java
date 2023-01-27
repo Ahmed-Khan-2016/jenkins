800110616ff6196141a32e94a6019627afe725e2
Ahmed Khan <ahmed.kahn@mywidz.se>
2023-01-27 14:31:57 +0100

    Update DemoClass.java

--- DemoClass.java
+++ DemoClass.java
@@ -2,9 +2,6 @@ public class DemoClass
 {   
 public static void main(String[] args)    
 {           
-System.out.print("Enter a string: ");   
-//reading a string from the user  
-String name = System.console().readLine();   
-System.out.println("You have entered: "+name);   
+System.out.print("Testing.....");   
 }   
 } 
