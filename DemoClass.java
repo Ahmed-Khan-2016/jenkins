ad1dfedecb7064b869a5529f578fe983cf38d6b2
Ahmed Khan <ahmed.kahn@mywidz.se>
2023-01-27 14:24:01 +0100

    Create DemoClass.java

---
+++ DemoClass.java
@@ -0,0 +1,10 @@
+public class DemoClass  
+{   
+public static void main(String[] args)    
+{           
+System.out.print("Enter a string: ");   
+//reading a string from the user  
+String name = System.console().readLine();   
+System.out.println("You have entered: "+name);   
+}   
+} 
